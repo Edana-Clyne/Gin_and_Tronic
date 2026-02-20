db89626950b64f04d57c5b4678320e5e8b227ca8
Edana Clyne <sclyne@ualberta.ca>
2026-02-20 14:31:43 -0700

    Deleted index file, created map.html

--- script.js
+++ script.js
@@ -4,7 +4,5 @@ var map = L.map('map').setView([53.5461, -113.4938], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-// A test marker for the Alberta Legislature Building
-L.marker([53.5333, -113.5065]).addTo(map)
-    .bindPopup('Alberta Legislature Building<br>Accessible Entrance on West Side.')
-    .openPopup();
+
+
